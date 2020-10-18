860acb9b46f6eab6fe9ad085173502a64f4c865c
Heishiro <heishiro.kanagawa@gmail.com>
2020-10-18 08:14:45 +0100

    add a discrete implicit model

--- scem/net.py
+++ scem/net.py
@@ -22,3 +22,32 @@ class TwoLayerFC(nn.Module):
         
     def forward(self, X):
         return self.module(X)
+
+
+class MultipleLinear(nn.Module):
+    
+    def __init__(self, in_features: int, out_features: int, n_linears: int,
+                 bias=True
+                 ):
+        super(MultipleLinear, self).__init__()
+        self.in_features = in_features
+        self.out_features = out_features
+        self.n_linears = n_linears
+        self.weight = nn.Parameter(
+            torch.Tensor(in_features, n_linears, out_features))
+        self.weight = nn.init.normal_(self.weight) 
+        if bias:
+            self.bias = nn.Parameter(torch.Tensor(n_linears, out_features))
+            self.bias = nn.init.zeros_(self.bias)
+        else:
+            # You should always register all possible parameters, but the
+            # optional ones can be None if you want.
+            self.register_parameter('bias', None)
+        
+    def forward(self, X):
+        W = self.weight
+        b = (0. if self.bias is None
+             else self.bias)
+        if len(X.shape) == 2:
+            return torch.einsum('bi,ijk->bjk', X, W) + b
+        return torch.einsum('nbi, ijk->nbjk', X, W)
